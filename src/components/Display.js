d96aed3724bc8a80fd5e14ff7696bb828702fdb2
yashchauhan008 <yashchauhan1775@gmail.com>
2025-03-18 18:31:18 +0530

    risky routing for display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Display = () => {
+const Display = ({ category, subcategory }) => {
   return (
-    <div className='display'>Display</div>
+    <div className='display'>
+      <h2>Category: {category}</h2>
+      <h3>Subcategory: {subcategory}</h3>
+    </div>
   )
 }
 
--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import Display from '../components/Display'
 
 const CategoryPage = () => {
-  return (
-    <>
-    <div className='full-screen'>
-        <Header/>
-        <div className='category-container'>
-        <Sidebar/>
-        <Display/>
+    const { category, subcategory } = useParams()
+
+    return (
+        <>
+        <div className='full-screen'>
+            <Header/>
+            <div className='category-container'>
+            <Sidebar/>
+            <Display category={category} subcategory={subcategory}/>
+            </div>
         </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default CategoryPage
